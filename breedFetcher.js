41e2bf9cac15552ed5eac31b22e535cacd02c7ab
spiritxhx <spiritxhx@gmail.com>
2019-07-04 13:40:40 -0400

    added the 2nd mocha test query

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,20 +2,18 @@ const request = require('request');
 
 const args = process.argv.slice(2);
 const fetchBreedDescription = (name, callback) => {
-
+  let err, data;
   let url = `https://api.thecatapi.com/v1/breeds/search?q=${name}`;
   request(url, (error, respond, body) => {
     if (error) {
-  
       return console.log(error);
     }
     if (body === "[]") {
-      console.log(`${name} Not Found!`);
+      err = (`${name} Not Found!`);
+      callback(err, "");
     } else {
-      const data = JSON.parse(body);
-      // console.log(typeof (data));
-      // console.log(data[0].description);
-      callback(error, data[0].description);
+      data = JSON.parse(body);
+      callback(err, data[0].description);
     }
   });
 };
--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -2,10 +2,18 @@ const { fetchBreedDescription } = require('../breedFetcher');
 const { assert } = require('chai');
 
 describe('fetchBreedDescription', () => {
-  it('returns a string description for a valid breed, cia callback', (done) => {
+  it('returns a string description for a valid breed, via callback', (done) => {
     fetchBreedDescription('Siberian', (err, desc) => {
       assert.equal(err, null);
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ";
+      assert.equal(expectedDesc, desc);
+      done();
+    });
+  });
+  it('returns a string description for a invalid breed, via callback', (done) => {
+    fetchBreedDescription('Siberianhahaha', (err, desc) => {
+      assert.equal(err, "Siberianhahaha Not Found!");
+      const expectedDesc = "";
       assert.equal(expectedDesc, desc);
       done();
     });
